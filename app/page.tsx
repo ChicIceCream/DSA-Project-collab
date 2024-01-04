a414a3c6710691c219a2ab5b30f493181f18a155
Armaan48985 <armaanthakur2308@gmail.com>
2024-01-04 22:40:52 +0530

    sdf

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import './globals.css'
 import Loading from "./Loading";
 import { Router, useRouter } from "next/router";
 import Hero from "./hero/page";
-
+import { edgeServerAppPaths } from "next/dist/build/webpack/plugins/pages-manifest-plugin";
 
 export default function Home() {
   const [loading, setLoading] = useState(true);
